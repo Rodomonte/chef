4c7eb22e009cd6ac5ebcee7782d048eb3e35b19f
Ryan Bell <rcb0000@gmail.com>
2019-09-08 12:03:54 -0600

    added mdiv to temp

--- TEMP.c
+++ TEMP.c
@@ -30,7 +30,23 @@ int max(int a, int b){ return (a > b) ? a : b; }
 
 int madd(int a, int b){ return ((a += b) >= MOD) ? a-MOD : a; }
 int msub(int a, int b){ return ((a -= b) < 0) ? a+MOD : a; }
-int mmul(int a, int b){ return (ll)a * b % MOD; }
+int mmul(int a, int b){ return (int)((ll)a * b % MOD); }
+
+int gcde(int a, int b, int* x, int* y){
+  if(!a){ *x = 0, *y = 1; return b; }
+  int g,x1,y1;
+  g = gcde(b % a, a, x1, y1), *x = y1 - (b / a) * x1, *y = x1;
+  return g;
+}
+int minv(int a){
+  int x,y;
+  if(gcde(a, MOD, &x, &y) != 1) a /= 0;
+  return (x % MOD + MOD) % MOD;
+}
+int mdiv(int a, int b){
+  int inv = minv(b);
+  return mmul(inv, a);
+}
 
 // Negative integer input
 #define gc getchar_unlocked
@@ -67,13 +83,9 @@ int pow(ll n, int e){
 }
 
 // Greatest common denominator
-int gcd(int a, int b){
-  return b ? gcd(b,a%b) : a;
-}
+int gcd(int a, int b){ return b ? gcd(b,a%b) : a; }
 // Least common multiple
-int lcm(int a, int b){
-  return a / gcd(a,b) * b;
-}
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
 
 // nCk recurrence relation
 double nck[40][40]={{0}};
@@ -82,23 +94,6 @@ for(i = 1; i < 40; i++){
   nck[i][0] = 1;
   for(j = 1; j <= i; j++)
     nck[i][j] = nck[i-1][j-1] + nck[i-1][j];
-}
-
-// Divisors
-int pf[1000001][256], pn[1000001];
-for(i = 0; i <= 1000000; ++i) pn[i] = pf[i][0] = 1;
-for(i = 2; i <= 1000000; ++i)
-  for(j = i; j <= 1000000; j += i)
-    pf[j][pn[j]++] = i;
-
-// Sieve
-char s[1000001]={0};
-int p[78498], pn;
-for(i = 2, pn = 0; i <= 1000000; ++i){
-  if(s[i]) continue;
-  for(j = (i<<1); j <= 1000000; j += i)
-    s[i] = 1;
-  p[pn++] = i;
 }
 
 // Quick sort
@@ -113,7 +108,7 @@ void sort(int* a, int n){
   sort(a, r-a+1), sort(l, a+n-l);
 }
 
-// FWT
+// BIT
 int N, li[20],ln,an;
 ll a[2000000];
 
@@ -129,27 +124,10 @@ void build(){
   an = j;
 }
 
-
-// Need push fn
-void update(int L, int R, int v){
-  int i,j,l,e,ia,ib;
-  l = 0, e = 1, i = L;
-  while(L <= R){
-    if(i != an-1 && i != li[l+1]-1){
-      j = li[l+1] + (L >> l+1);
-      ia = (j - li[l+1]) << (l+1), ib = ia + (e << 1) - 1;
-      if(ia == L && ib <= R){ i = j, e <<= 1, l++; continue; }
-    }
-    ia = (i - li[l]) << l;
-    if(ia == L){
-      a[i] += v;
-      push(i);
-      L += e;
-      if(L > R) break;
-    }
-    if(R - L + 1 < e) e >>= 1, l--, i = li[l] + (ia >> l) + 1;
-    else i++;
-  }
+void update(int i, int v){
+  int l;
+  l = 1, a[i] += v; //!
+  while(l < ln) a[(i >>= 1) + li[l++]] += v; //!
 }
 
 ll query(int L, int R){
@@ -177,30 +155,30 @@ ll query(int L, int R){
 // Hashing
 int KMOD; // = N/.75 (# of hash buckets)
 int key(char* s){
-  // MURMUR
-  int l,m,r,h,l4,i,i4,k;
-  l = strlen(s), m = 0x5bd1e995, r = 24;
-  h = 0x9747b28c ^ l, l4 = l / 4;
-  for(i = 0; i < l4; i++){
-    i4 = i * 4;
-    k = (s[i4] & 0xff);
-    k += ((s[i4+1] & 0xff) << 8);
-    k += ((s[i4+2] & 0xff) << 16);
-    k += ((s[i4+3] & 0xff) << 24);
-    k *= m;
-    k ^= k >> r;
-    k *= m;
-    h *= m;
-    h ^= k;
-  }
-  switch(l % 4){
-  case 3: h ^= (s[(l&~3)+2] & 0xff) << 16;
-  case 2: h ^= (s[(l&~3)+1] & 0xff) << 8;
-  case 1: h ^= (s[l&~3] & 0xff);
-    h *= m;
-  }
-  h ^= h >> 13;
-  h *= m;
-  h ^= h >> 15;
-  return h % KMOD;
+	// MURMUR
+	int l,m,r,h,l4,i,i4,k;
+	l = strlen(s), m = 0x5bd1e995, r = 24;
+	h = 0x9747b28c ^ l, l4 = l / 4;
+	for(i = 0; i < l4; i++){
+		i4 = i * 4;
+		k = (s[i4] & 0xff);
+		k += ((s[i4+1] & 0xff) << 8);
+		k += ((s[i4+2] & 0xff) << 16);
+		k += ((s[i4+3] & 0xff) << 24);
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+		h *= m;
+		h ^= k;
+	}
+	switch(l % 4){
+	case 3: h ^= (s[(l&~3)+2] & 0xff) << 16;
+	case 2: h ^= (s[(l&~3)+1] & 0xff) << 8;
+	case 1: h ^= (s[l&~3] & 0xff);
+			h *= m;
+	}
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+	return h % KMOD;
 }
